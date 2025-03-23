3970e22ac5e469b2206f2ca04de4b95ce592cc3c
chandan chauhan <chandanmau2018@gmail.com>
2025-03-23 15:09:47 +0530

    added

--- websocket/src/main/java/com/impl/webhook/WebSocketApplication.java
+++ websocket/src/main/java/com/impl/webhook/WebSocketApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class WebhookApplication {
+public class WebSocketApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(WebhookApplication.class, args);
